b8a46d03f632ccc2eb33060d4aa55bbf850dfd67
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-01-10 22:51:30 +0700

    add: menambahkan ci

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Nama pengguna GitHub
+GITHUB_USERNAME="abiyamakruf"
+
+# Nama image
+IMAGE_NAME="karsajobs"
+IMAGE_TAG="latest"
+
+# URL GitHub Container Registry
+REGISTRY="ghcr.io"
+
+# Nama package
+GITHUB_IMAGE="$REGISTRY/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+
+# Build Docker image
+echo "Building Docker image for karsajobs..."
+docker build -t $GITHUB_IMAGE .
+
+# Login ke GitHub Container Registry
+echo "Logging in to GitHub Container Registry..."
+echo $PAT | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+
+# Push Docker image ke GitHub Container Registry
+echo "Pushing Docker image to GitHub Packages..."
+docker push $GITHUB_IMAGE
+
+echo "Docker image pushed successfully to GitHub Packages!"
